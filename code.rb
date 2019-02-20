b5e1525339bca1c2cc54b51e73a6120ce59da418
krishnakeerthi <krishnakeerthi675@gmail.com>
2019-02-20 08:15:43 -0500

    Initial commit

---
+++ code.rb
@@ -0,0 +1,19 @@
+# YOUR CODE HERE
+input =1
+puts input
+while input <100 do
+  input = input+1
+ if (input%2==1)
+  puts input
+else
+end
+end
+
+#another approach
+#------------------------
+#input=1
+#puts input
+#while input <=98 do
+#  input= input+2
+#  puts input
+#end
